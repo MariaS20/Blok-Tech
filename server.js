bc00c449978021b842d930b271ece321ba4dab4c
MariaS20 <tess.smit@hva.nl>
2020-02-27 12:32:59 +0100

    message

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.static('public'))
+
+app.get( "*", (req, res) => {
+    res.send("Error! 404 this route doesn't exist");
+} );
+
+var http = require('http')
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
