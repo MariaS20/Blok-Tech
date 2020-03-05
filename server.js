43914e4e1842544acd4bd9d2c503e007b57604ff
MariaS20 <tess.smit@hva.nl>
2020-03-05 11:49:48 +0100

    test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var http = require('http')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example is app listening on port ${port}!`))
 
 
 
