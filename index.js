27d79b5dcccb0d7aa57945b2bd9b0e330592191e
MariaS20 <tess.smit@hva.nl>
2020-03-05 11:59:01 +0100

    tes

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var movies = [
+{
+    id: 'guardians-of-the-galaxy',
+    title: 'Guardians of the galaxy',
+    plot: 'Five friends try to save the galaxy from the bad guy.',
+    description: 'When the universe is in danger, 5 people team up together to save the galaxy from eternal doom.'
+
+},
+{
+    id: 'knives-out',
+    title: 'Knives Out',
+    plot: 'A murder 2 turns into a suicide and a detective is here to solve the mystery.',
+    description: 'When is the great Trombey passes away, it seems like it was more than suicide.  '
+
+}
+
+] 
