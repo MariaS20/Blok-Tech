ff2f6c4600bdc902b01ddfb503079b5fbfd212dc
MariaS20 <tess.smit@hva.nl>
2020-06-28 08:41:52 +0200

    kleine aanpassingen

--- server.js
+++ server.js
@@ -10,15 +10,17 @@ require('dotenv').config()
 
 
 //connect met de database
+
 let db = null
 const url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
 
-mongo.MongoClient.connect(url, function (err, client) {
+mongo.MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
   if (err) {
     throw err
   }
-
+  
   db = client.db(process.env.DB_NAME)
+  
 })
 
 express()
